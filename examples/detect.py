69586d6323805cd3d4c24f7451b8939de8ca702a
wqliu <528991630@qq.com>
2021-09-03 00:18:28 +0800

    update detect file and add some new images

--- examples/detect.py
+++ examples/detect.py
@@ -2,10 +2,13 @@ import cv2
 import sys
 import os.path
 
-def detect(filename, cascade_file = "../lbpcascade_animeface.xml"):
+filepath = "/home/wqliu/Workplace/lbpcascade_animeface/examples/inputimage"
+fileoutpath = "/home/wqliu/Workplace/lbpcascade_animeface/examples/outimage"
+imagepath = os.listdir(filepath)
+
+def detect(image1,filename, cascade_file = "../lbpcascade_animeface.xml"):
     if not os.path.isfile(cascade_file):
         raise RuntimeError("%s: not found" % cascade_file)
-
     cascade = cv2.CascadeClassifier(cascade_file)
     image = cv2.imread(filename, cv2.IMREAD_COLOR)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -14,17 +17,25 @@ def detect(filename, cascade_file = "../lbpcascade_animeface.xml"):
     faces = cascade.detectMultiScale(gray,
                                      # detector options
                                      scaleFactor = 1.1,
-                                     minNeighbors = 5,
+                                     minNeighbors = 3,
                                      minSize = (24, 24))
     for (x, y, w, h) in faces:
         cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
-    cv2.imshow("AnimeFaceDetect", image)
-    cv2.waitKey(0)
-    cv2.imwrite("out.png", image)
+    #cv2.imshow("AnimeFaceDetect", image)
+    #cv2.waitKey(0)
+    if(len(faces) == 0):
+        a=1
+    else:
+        out = os.path.join(fileoutpath,image1)
+        cv2.imwrite(out, image)
 
-if len(sys.argv) != 2:
-    sys.stderr.write("usage: detect.py <filename>\n")
-    sys.exit(-1)
+
+for images in imagepath:
+    image = os.path.join(filepath,images)
+    detect(images,image)
+#if len(sys.argv) != 2:
+    #sys.stderr.write("usage: detect.py <filename>\n")
+    #sys.exit(-1)
     
-detect(sys.argv[1])
+#detect(sys.argv[1])
